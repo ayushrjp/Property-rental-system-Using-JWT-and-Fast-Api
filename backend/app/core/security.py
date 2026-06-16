0c77a17d7e56560d717fba5c846fece70c89c917
ayush <ayushrjp2005@gmail.com>
2026-06-16 15:24:33 +0530

    fix JWT token error

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,15 +1,17 @@
-import os
 from datetime import datetime, timedelta
 from jose import jwt
-from dotenv import load_dotenv
 
-load_dotenv()
+SECRET_KEY = "your_secret_key_123"   # ⚠️ change later
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=60)
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
